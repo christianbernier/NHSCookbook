09486de0c5791e504c6d9c4a4ebb8b782bbc8b8b
christianbernier <64christianb@gmail.com>
2021-03-05 20:25:47 -0500

    Analytics data

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,32 @@
 module.exports = {
   plugins: [
     "gatsby-plugin-emotion",
-    "@rhysforyou/gatsby-plugin-react-helmet-async"
+    "@rhysforyou/gatsby-plugin-react-helmet-async",
+    {
+      resolve: `gatsby-plugin-google-gtag`,
+      options: {
+        // You can add multiple tracking ids and a pageview event will be fired for all of them.
+        trackingIds: [
+          "G-7V0HXSX7LW", // Google Analytics / GA
+          "G-81WP1PPMEP"
+        ],
+        // This object gets passed directly to the gtag config command
+        // This config will be shared across all trackingIds
+        gtagConfig: {
+          optimize_id: "G-7V0HXSX7LW",
+          anonymize_ip: true,
+          cookie_expires: 0,
+        },
+        // This object is used for configuration specific to this plugin
+        pluginConfig: {
+          // Puts tracking script in the head instead of the body
+          head: false,
+          // Setting this parameter is also optional
+          respectDNT: true,
+          // Avoids sending pageview hits from custom paths
+          exclude: ["/preview/**", "/do-not-track/me/too/"],
+        },
+      },
+    },
   ]
 }
